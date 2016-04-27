0c3389386b608dafe29bceb3a5e3b933e8c1eb3c
Lenny <lenny.donnez@gmail.com>
2016-04-27 22:40:29 +0200

    Formatting

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,15 +2,15 @@ class Photo < ActiveRecord::Base
   before_create :default_name
 
   has_and_belongs_to_many :projects
-  has_attached_file :photo, :styles => { :thumbnail => "50x50", :medium => "400x400!", :large => "1000x666!" }, 
-      :default_url => "/images/:style/missing.png",
-      :convert_options => { :medium => "-quality 75 -strip",
-                            :large => "-quality 75 -strip" }
+  has_attached_file :photo, styles: { thumbnail: "50x50", medium: "400x400!", large: "1000x666!" },
+                            default_url: "/images/:style/missing.png",
+                            convert_options: { medium: "-quality 75 -strip",
+                                               large: "-quality 75 -strip" }
 
-    validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
-    #validates_with AttachmentSizeValidator, :attributes => :photo, :less_than => 1.megabytes
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+  # validates_with AttachmentSizeValidator, :attributes => :photo, :less_than => 1.megabytes
 
-    def default_name
-        self.title = File.basename(photo_file_name, '.*').titleize if self.title.empty? 
-    end
+  def default_name
+    self.title = File.basename(photo_file_name, ".*").titleize if title.nil?
+  end
 end
