efbefcdda0fd3fd1dd27bf3c5bcedd6a4bcd7e15
Lenny <lenny.donnez@gmail.com>
2016-04-29 00:04:55 +0200

    Add basic menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,44 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
 //= require_tree .
+var ready = function() {
+  $(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+      $('.main-menu').addClass('sticky');
+    } else {
+      $('.main-menu').removeClass('sticky');
+    }
+  });
+
+  $('.mobile-toggle').click(function() {
+    if ($('.main-menu').hasClass('open-nav')) {
+      $('.main-menu').removeClass('open-nav');
+    } else {
+      $('.main-menu').addClass('open-nav');
+    }
+  });
+
+  $('.main-menu li a').click(function() {
+    if ($('.main-menu').hasClass('open-nav')) {
+      $('.navigation').removeClass('open-nav');
+      $('.main-menu').removeClass('open-nav');
+    }
+  });
+
+  $('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+      scrollTop: target
+    }, 500);
+    event.preventDefault();
+  });
+}
+$(document).ready(ready);
+$(document).on('page:load', ready);
+$(function(){ $(document).foundation(); });
