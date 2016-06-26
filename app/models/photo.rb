7f2542860b316221a9d97a47fb29054bfc9a2cfd
Lenny <lenny.donnez@gmail.com>
2016-06-26 15:23:19 +0200

    Generate better thumbnail

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ActiveRecord::Base
   before_create :default_name
 
   has_and_belongs_to_many :galleries
-  has_attached_file :photo, styles: { thumbnail: "50x50", medium: "400x400!", large: "1000x666!" },
+  has_attached_file :photo, styles: { thumbnail: "200x200#", medium: "400x400!", large: "1000x666!" },
                             default_url: "/images/:style/missing.png",
                             convert_options: { medium: "-quality 75 -strip",
                                                large: "-quality 75 -strip" }
