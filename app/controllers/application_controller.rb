810bd56aa0c642e22ecd57a020271bc1120d04d7
Lenny <lenny.donnez@gmail.com>
2016-07-06 21:01:09 +0200

    Make Settings available in whole app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_contact
+  before_action :set_global_resources
 
-  def set_contact
-    @contact = Contact.first
+  def set_global_resources
+    @settings = Setting.first
   end
 end
