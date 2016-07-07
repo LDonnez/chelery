66a17d5417ba089ea64e8afc7ff2c3c9834a8d51
Lenny <lenny.donnez@gmail.com>
2016-07-07 21:19:01 +0200

    Fix for turbolinks

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,37 +1,33 @@
-var ready = function() {
-  $(window).scroll(function() {
+$(window).scroll(function() {
 
-    if ($(window).scrollTop() > 50) {
-      $('.main-menu').addClass('sticky');
-    } else {
-      $('.main-menu').removeClass('sticky');
-    }
-  });
+  if ($(window).scrollTop() > 50) {
+    $('.main-menu').addClass('sticky');
+  } else {
+    $('.main-menu').removeClass('sticky');
+  }
+});
 
-  $('.mobile-toggle').click(function() {
-    if ($('.main-menu').hasClass('open-nav')) {
-      $('.main-menu').removeClass('open-nav');
-    } else {
-      $('.main-menu').addClass('open-nav');
-    }
-  });
+$('.mobile-toggle').on('click', function() {
+  if ($('.main-menu').hasClass('open-nav')) {
+    $('.main-menu').removeClass('open-nav');
+  } else {
+    $('.main-menu').addClass('open-nav');
+  }
+});
 
-  $('.main-menu li a').click(function() {
-    if ($('.main-menu').hasClass('open-nav')) {
-      $('.navigation').removeClass('open-nav');
-      $('.main-menu').removeClass('open-nav');
-    }
-  });
+$('.main-menu li a').on('click', function() {
+  if ($('.main-menu').hasClass('open-nav')) {
+    $('.navigation').removeClass('open-nav');
+    $('.main-menu').removeClass('open-nav');
+  }
+});
 
-  $('nav a').click(function(event) {
-    var id = $(this).attr("href");
-    var offset = 70;
-    var target = $(id).offset().top - offset;
-    $('html, body').animate({
-      scrollTop: target
-    }, 500);
-    event.preventDefault();
-  });
-}
-$(document).ready(ready);
-$(document).on('page:load', ready);
+$('nav a').on('click', function(event) {
+  var id = $(this).attr("href");
+  var offset = 70;
+  var target = $(id).offset().top - offset;
+  $('html, body').animate({
+    scrollTop: target
+  }, 500);
+  event.preventDefault();
+});
