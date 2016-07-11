c5c6aae500b0833ad43b7c3796b788fc660cb4b3
Lenny <lenny.donnez@gmail.com>
2016-07-11 19:41:18 +0200

    Make menu fixed

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,37 +1,28 @@
 var ready = function() {
-$(window).scroll(function() {
+  $('.mobile-toggle').click(function() {
+    if ($('.main-menu').hasClass('open-nav')) {
+      $('.main-menu').removeClass('open-nav');
+    } else {
+      $('.main-menu').addClass('open-nav');
+    }
+  });
 
-  if ($(window).scrollTop() > 50) {
-    $('.main-menu').addClass('sticky');
-  } else {
-    $('.main-menu').removeClass('sticky');
-  }
-});
+  $('.main-menu li a').click(function() {
+    if ($('.main-menu').hasClass('open-nav')) {
+      $('.navigation').removeClass('open-nav');
+      $('.main-menu').removeClass('open-nav');
+    }
+  });
 
-$('.mobile-toggle').on('click', function() {
-  if ($('.main-menu').hasClass('open-nav')) {
-    $('.main-menu').removeClass('open-nav');
-  } else {
-    $('.main-menu').addClass('open-nav');
-  }
-});
-
-$('.main-menu li a').on('click', function() {
-  if ($('.main-menu').hasClass('open-nav')) {
-    $('.navigation').removeClass('open-nav');
-    $('.main-menu').removeClass('open-nav');
-  }
-});
-
-$('nav a').on('click', function(event) {
-  var id = $(this).attr("href");
-  var offset = 70;
-  var target = $(id).offset().top - offset;
-  $('html, body').animate({
-    scrollTop: target
-  }, 500);
-  event.preventDefault();
-});
+  $('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+      scrollTop: target
+    }, 500);
+    event.preventDefault();
+  });
 }
 $(document).ready(ready);
 $(document).on('page:load', ready);
