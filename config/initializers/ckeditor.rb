cd12f7bc8b2da1019894d2b71e0637a7f62509b1
Lenny <lenny.donnez@gmail.com>
2016-10-07 00:34:19 +0200

    Fix syntax

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -6,10 +6,8 @@ Ckeditor.setup do |config|
   # available as additional gems.
   require "ckeditor/orm/active_record"
 
-  Ckeditor.setup do |config|
     # //cdn.ckeditor.com/<version.number>/<distribution>/ckeditor.js
-    config.cdn_url = "//cdn.ckeditor.com/4.5.6/standard/ckeditor.js"
-  end
+  config.cdn_url = "//cdn.ckeditor.com/4.5.10/standard/ckeditor.js"
   # Allowed image file types for upload.
   # Set to nil or [] (empty array) for all file types
   # By default: %w(jpg jpeg png gif tiff)
