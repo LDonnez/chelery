913924a640088387f1f25569fdc6e92666a2ddbc
Lenny <lenny.donnez@gmail.com>
2016-11-02 20:03:15 +0100

    Use friendly.find

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -65,7 +65,8 @@ class GalleriesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_gallery
-      @gallery = Gallery.find(params[:id])
+      #When friendlyID gem updates version change this again to Gallery.find(params[:id])
+      @gallery = Gallery.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
